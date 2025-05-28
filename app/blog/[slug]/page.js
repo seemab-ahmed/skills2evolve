1632a74a5f1fe761317eb2833a9b05490497c177
seemabwork <seemab.work06@gmail.com>
2025-05-28 04:22:36 -0700

    create blogdetail page

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+"use client";
+import BlogdetailComponent from '@/component/Blogdetail';
+
+const Blogdetail = () => {
+  return (
+    <>
+      <BlogdetailComponent />
+    </>
+  );
+};
+
+export default Blogdetail;
